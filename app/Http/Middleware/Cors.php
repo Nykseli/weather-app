255ff2c08d107dc72e3bb3a8a22a58d7b97ada97
Miika Alikirri <miika.alik@gmail.com>
2019-09-10 16:57:52 +0300

    Added cors to auth

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,8 +16,8 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+            //->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
-
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+header('Access-Control-Allow-Credentials:  true');
 use Illuminate\Http\Request;
 use App\Http\Controllers\WeatherController;
 
@@ -20,7 +23,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 // Login needs to be name as login so laravel know how to redirect
-Route::post('login', ['as' => 'login', 'uses' => 'LoginController@do']);
+Route::post('login', ['as' => 'login', 'uses' => 'Auth\LoginController@login']);
 Route::post('logout', 'Auth\LoginController@logout');
 Route::post('register', 'Auth\RegisterController@register');
 
